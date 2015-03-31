f423476668394074d509397a2dbd02ad59f9511a
Radzislaw Galler <r.galler@paycasso.com>
2015-03-31 10:45:15 +0200

    Updated personal hooks

--- personal/hooks.el
+++ personal/hooks.el
@@ -1,3 +1,7 @@
 (add-hook 'term-mode-hook
           '(lambda ()
              (setq yas-dont-activate t)))
+
+(add-hook 'scala-mode-hook (lambda ()
+                             (require 'ensime)
+                             (ensime-mode)))
